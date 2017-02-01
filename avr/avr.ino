ea85456f0dced400449524c7326ba6c52a500111
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2017-02-01 22:53:48 +0200

    Make the AVR code mode readable by adding comments and organizing a bit

--- avr/avr.ino
+++ avr/avr.ino
@@ -1,5 +1,4 @@
-
-/*
+/* -*- mode: c++; c-file-style: "linux" -*-
   Telepulssi TP-KN10 slave LED screen driver
 */
 
@@ -18,6 +17,9 @@ static uint8_t buf_b[sizeof(buf_a)];
 
 uint8_t *buf_front = buf_a;
 uint8_t *buf_back = buf_b;
+uint8_t col_i = 0;
+uint8_t row_i = 0;
+int buf_i = 0;
 
 void buf_swap(void) {
 	uint8_t *tmp = buf_front;
@@ -37,35 +39,35 @@ void setup() {
 	Timer1.attachInterrupt(driveDisplay);
 }
 
-uint8_t col_i = 0;
-uint8_t row_i = 0;
-int buf_i = 0;
-
 void loop() {
-	if (Serial.available() > 0) {
-                // read the incoming byte
-                uint8_t data = Serial.read();
-		if (data >= 0x20) {
-			// If sending escape, reset buffer to beginning
-			buf_i = 0;
-			Serial.write('R');
-			return;
-		}
-		
-		buf_back[buf_i++] = data;
-		if (buf_i == sizeof(buf_a)) {
-			buf_i = 0;
-			buf_swap();
-			Serial.write('K');
-		}
-        }
+	// Get a byte from serial port if available.
+	if (!Serial.available()) return;
+	uint8_t data = Serial.read();
+	
+	// In case of an escape, stop receiving current frame.
+	if (data >= 0x20) {
+		buf_i = 0;
+		Serial.write('R');
+		return;
+	}
+
+	// Store the data to back buffer
+	buf_back[buf_i++] = data;
+	
+	// If we have reached the end of frame, flip buffers
+	if (buf_i == sizeof(buf_a)) {
+		buf_i = 0;
+		buf_swap();
+		Serial.write('K');
+	}
 }
 
 void driveDisplay() {
-	// Main screen turn off
+	// Main screen turn off.
 	PORTC = 0xFF;
 	PORTD = 0xFF; // Latch data on pin 5 at the same time
 
+	// Switch the row on row driver if needed
 	if (col_i == 0) {
 		SPI.transfer(1 << row_i);
 
@@ -74,26 +76,29 @@ void driveDisplay() {
 		digitalWrite(4, HIGH);
 	}
 	
-	// Main screen turn on.
+	// Main screen turn on!
 	if (col_i < 2) {
 		PORTD = ~((1 << 6) << col_i);
 	} else {
 		PORTC = ~(1 << (col_i-2));
 	}
 	
-	// Prepare new data
-	col_i++;
+	// After turning on the screen, we have "plenty" of CPU cycles
+	// to spend. Preparing a new segment to display.
 	
-	// Advance to next row if needed
+	// Go to the next column. If reached the end of line, jump to next.
+	col_i++;
 	if (col_i > 7) {
 		col_i = 0;
 		row_i++;
 		if (row_i > 6) row_i = 0;
 	}
 
-	// Pull down latch
+	// Column driver latches on rising edge so we can already pull
+	// it down.
 	digitalWrite(5, LOW);
 	
-	// Send new data to SPI. Don't wait it to complete
+	// Send new data via SPI. Don't need to wait it to complete
+	// because it completes before the next timer interrupt.
 	SPDR = buf_front[8*row_i+col_i];
 }
