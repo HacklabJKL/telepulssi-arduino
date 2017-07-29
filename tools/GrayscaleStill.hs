8f0788c3c7a19ac04ea6d2e1a702088db6fd3316
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2017-07-29 12:12:09 +0300

    Clean-up 2-bit grayscale generator and add serial command

--- tools/GrayscaleStill.hs
+++ tools/GrayscaleStill.hs
@@ -17,20 +17,28 @@ binarize _ _ _ v = putWord8 2 $ v `shiftR` 6
 -- |Elovalo escape style.
 escape = B.intercalate "~\0" . B.split '~'
 
+-- |Converts image to 8-bit grayscale.
+dynToGrayscale :: DynamicImage -> Image Pixel8
+dynToGrayscale img = case img of
+  -- Some shortcuts
+  ImageY8  a -> extractLumaPlane a
+  ImageYA8 a -> extractLumaPlane a
+  -- And generic form
+  a          -> extractLumaPlane $ convertRGB8 a
+
 -- |Load image and return pixel in serial format.
 telepulssify f = do
-  imgDyn <- readImage f >>= either fail return
-  let img = case imgDyn of
-        ImageRGB8  a -> extractLumaPlane a
-        ImageRGBA8 a -> extractLumaPlane a
-        ImageY8    a -> extractLumaPlane a
-        ImageYA8   a -> extractLumaPlane a
-        _ -> error "Not grayscale image"
+  -- Read image directly to grayscle
+  img <- readImage f >>= either fail (return.dynToGrayscale)
+  -- Check dimensions
   when (imageWidth img /= 40 || imageHeight img /= 7) $ fail "Image must be 40×7"
-  return $ escape $ runPut $ runBitPut $ pixelFoldM binarize () img 
+  -- Generate raw binary 2-bit grayscale data
+  let binaryImg = runPut $ runBitPut $ pixelFoldM binarize () img
+  -- Produce final serial line data
+  return $ "~F" `B.append` escape binaryImg
 
 main = do
   args <- getArgs
   case args of
     [f] -> telepulssify f >>= B.putStr
-    _ -> fail "Image file required"
+    _   -> fail "Image file required"
